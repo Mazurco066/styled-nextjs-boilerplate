9ce0ba7b5010da4dca62989bbb999bd0e51f25fa
Gabriel Ribeiro <mazurco066@gmail.com>
2021-03-28 11:47:24 -0300

    feat(toast): config react toastfy

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 // Next dependencies
 import Head from 'next/head'
 import PropTypes from 'prop-types'
+import { ToastContainer } from 'react-toastify'
 
 // App styles
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles, { theme } from 'styles/global'
+import 'react-toastify/dist/ReactToastify.css'
 
 // App Function
 export default function App({ Component, pageProps }) {
@@ -15,13 +17,14 @@ export default function App({ Component, pageProps }) {
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#06092B" />
+        <meta name="theme-color" content="#bb86fc" />
         <meta name="description" content="Nextjs styled boilerplate" />
       </Head>
       <GlobalStyles />
       <ThemeProvider theme={theme}>
         <main id="layout">
           <Component {...pageProps} />
+          <ToastContainer position="bottom-right"/>
         </main>
       </ThemeProvider>
     </>
