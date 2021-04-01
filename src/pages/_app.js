9909c02368e387e7f643e18171e96876de5ac9e1
Gabriel Ribeiro <mazurco066@gmail.com>
2021-04-01 15:07:10 -0300

    feat(nprogress): added nprogess lib

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 // Next dependencies
 import Head from 'next/head'
+import NextNprogress from 'nextjs-progressbar'
 import PropTypes from 'prop-types'
 import { ToastContainer } from 'react-toastify'
 
@@ -16,9 +17,15 @@ export default function App({ Component, pageProps }) {
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#bb86fc" />
+        <meta name="theme-color" content="#e20e8d" />
         <meta name="description" content="Nextjs styled boilerplate" />
       </Head>
+      <NextNprogress
+        color="#e20e8d"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+      />
       <GlobalStyles />
       <Component {...pageProps} />
       <ToastContainer position="bottom-right"/>
